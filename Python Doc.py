bc631fc45d7a71bd1f2c828b64d679cf93326650
Emre SEZER <42117625+emresezer@users.noreply.github.com>
2025-10-14 16:50:33 +0300

    Update Python Doc.py
    gamma cas yıldızı 5. sırada eklendi

--- Python Doc.py
+++ Python Doc.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.colors import LinearSegmentedColormap
 
-luminosity = [1,25.4,120000,0.0017  ] 
-temperature = [5778,9940,3500,3000   ] 
-sizes = [10,17.1,85,1.4]
+luminosity = [1,25.4,120000,0.0017,17] 
+temperature = [5778,9940,3500,3000,25000] 
+sizes = [10,17.1,85,1.4,17]
 
 colors = ['red', 'orange', 'yellow', 'white', 'turquoise', 'blue', 'navy']
 custom_cmap = LinearSegmentedColormap.from_list('custom_cmap', colors)
